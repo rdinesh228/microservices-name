194e997d4150b40b4f3d58f3b884b38e5330d666
rdinesh228 <78357504+rdinesh228@users.noreply.github.com>
2025-04-05 22:25:39 +0530

    initial commit

---
+++ src/main/java/com/example/name/controller/NameController.java
@@ -0,0 +1,25 @@
+package com.example.name.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/name")
+public class NameController {
+	
+	@GetMapping(value = "/getname")
+	public String getName() {
+		return "Name from Name Service";
+	}
+	
+	@PostMapping(value = "/getname")
+	public String getNameForPost(@RequestBody String request) {
+		System.out.println("Request = "+ request);
+		return "Name from Name Service";
+	}
+	
+
+}
